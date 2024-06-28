350a976d9665f48a8999e329193810ffe9b1e19e
Mathieu Bellon <mathieu.bellon@gitguardian.com>
2024-06-28 16:30:23 +0200

    StatusbarItem OK

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let myStatusBarItem: vscode.StatusBarItem;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "decide-vscode" is now active!');
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+
+	// register a command that is invoked when the status bar
+	// item is selected
+	const myCommandId = 'sample.showSelectionCount';
+	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+		const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+		vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`);
+	}));
+
+	// create a new status bar item that we can now manage
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = myCommandId;
+	subscriptions.push(myStatusBarItem);
+
+	// register some listener that make sure the status bar 
+	// item always up-to-date
+	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+
+	// update status bar item once at start
+	updateStatusBarItem();
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -19,8 +35,27 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from super decide-vscode!');
 	});
 
-	context.subscriptions.push(disposable);
+	subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function updateStatusBarItem(): void {
+	const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+	if (n > 0) {
+		myStatusBarItem.text = `Decide: ${n} secrets found!`;
+		myStatusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
+	} else {
+		myStatusBarItem.text = `Decide: ready to find secrets!`;
+		myStatusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.activeBackground');
+	}
+	myStatusBarItem.show();
+}
+
+function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
+	let lines = 0;
+	if (editor) {
+		lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
+	}
+	return lines;
+}
+
 export function deactivate() { }
